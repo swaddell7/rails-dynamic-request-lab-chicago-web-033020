def30e8ae739e35df3705bbcf0b5d6260966a129
swaddell7 <stefani.waddell@hotmail.com>
2020-04-25 16:35:50 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render 'index'
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render 'show'
   end
 end
